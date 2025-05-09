e39475d28eca938adf9bc39c7fec0b66da0f6c07
deenoloo <doreenoloo2@gmail.com>
2025-05-09 08:03:02 -0400

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, possible_anagrams):
+        return [word for word in possible_anagrams if self._is_anagram(word.lower())]
+    
+    def _is_anagram(self, candidate):
+        if candidate == self.word:
+            return False
+        return sorted(candidate) == sorted(self.word)
